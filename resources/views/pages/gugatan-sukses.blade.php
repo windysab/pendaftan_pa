48d39d47bf5d042d72cee84b935eb14deb8aeaa3
windys <windysabtami85@gmail.com>
2024-09-29 21:09:48 +0800

    okeew

--- resources/views/pages/gugatan-sukses.blade.php
+++ resources/views/pages/gugatan-sukses.blade.php
@@ -24,21 +24,36 @@
                     Terima kasih, data gugatan Anda telah berhasil disimpan.
                 </p>
 
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Data Gugatan</h4>
+                <div class="layout-container">
+                    <div class="left-column">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Data Gugatan</h4>
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        @foreach($gugatan->toArray() as $key => $value)
+                                            <tr>
+                                                <th>{{ ucfirst(str_replace('_', ' ', $key)) }}</th>
+                                                <td>{{ $value }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                @foreach($gugatan->toArray() as $key => $value)
-                                    <tr>
-                                        <th>{{ ucfirst(str_replace('_', ' ', $key)) }}</th>
-                                        <td>{{ $value }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="right-column">
+                        <!-- Tambahkan konten tambahan di sini -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Informasi Tambahan</h4>
+                            </div>
+                            <div class="card-body">
+                                <p>Anda dapat menambahkan informasi tambahan di sini.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,4 +65,3 @@
     <!-- JS Libraries -->
     <!-- Page Specific JS File -->
 @endpush
-
