6bc3b4ed8e3f98aae556ee3901484ed496518852
windysab <windysabtami85@gmail.com>
2024-09-30 16:13:49 +0800

    upadat

--- resources/views/pages/gugatan-sukses.blade.php
+++ resources/views/pages/gugatan-sukses.blade.php
@@ -25,33 +25,37 @@
                 </p>
 
                 <div class="layout-container">
-                    <div class="left-column">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Data Gugatan</h4>
-                            </div>
-                            <div class="card-body">
-                                <table class="table table-bordered">
-                                    <tbody>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Data Gugatan</h4>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
                                         @foreach($gugatan->toArray() as $key => $value)
-                                            <tr>
-                                                <th>{{ ucfirst(str_replace('_', ' ', $key)) }}</th>
-                                                <td>{{ $value }}</td>
-                                            </tr>
+                                            @if(!empty($value))
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label><strong>{{ ucfirst(str_replace('_', ' ', $key)) }}</strong></label>
+                                                        
+                                                    </div>
+                                                </div>
+                                            @endif
                                         @endforeach
-                                    </tbody>
-                                </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="right-column">
-                        <!-- Tambahkan konten tambahan di sini -->
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Informasi Tambahan</h4>
-                            </div>
-                            <div class="card-body">
-                                <p>Anda dapat menambahkan informasi tambahan di sini.</p>
+                        <div class="col-md-4">
+                            <!-- Tambahkan konten tambahan di sini -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Informasi Tambahan</h4>
+                                </div>
+                                <div class="card-body">
+                                    <p>Anda dapat menambahkan informasi tambahan di sini.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
