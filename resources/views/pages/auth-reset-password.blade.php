3f95f0d76177f261a924c8875f1d1cd05d001d90
windysab <windysabtami85@gmail.com>
2024-10-03 16:36:41 +0800

    oke lagi

--- resources/views/pages/auth-reset-password.blade.php
+++ resources/views/pages/auth-reset-password.blade.php
@@ -14,8 +14,7 @@
 
         <div class="card-body">
             <p class="text-muted">We will send a link to reset your password</p>
-            <form method="POST" action="{{ route('password.email') }}">
-                @csrf
+            <form method="POST">
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
@@ -28,10 +27,36 @@
                 </div>
 
                 <div class="form-group">
+                    <label for="password">New Password</label>
+                    <input id="password"
+                        type="password"
+                        class="form-control pwstrength"
+                        data-indicator="pwindicator"
+                        name="password"
+                        tabindex="2"
+                        required>
+                    <div id="pwindicator"
+                        class="pwindicator">
+                        <div class="bar"></div>
+                        <div class="label"></div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password-confirm">Confirm Password</label>
+                    <input id="password-confirm"
+                        type="password"
+                        class="form-control"
+                        name="confirm-password"
+                        tabindex="2"
+                        required>
+                </div>
+
+                <div class="form-group">
                     <button type="submit"
                         class="btn btn-primary btn-lg btn-block"
-                        tabindex="2">
-                        Send Reset Link
+                        tabindex="4">
+                        Reset Password
                     </button>
                 </div>
             </form>
@@ -41,4 +66,9 @@
 
 @push('scripts')
     <!-- JS Libraies -->
+    <script src="{{ asset('library/jquery.pwstrength/jquery.pwstrength.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/auth-register.js') }}"></script>
 @endpush
+
