b147e6ff3334920155c9feb0c4b58f2509747d32
windysab <windysabtami85@gmail.com>
2024-10-08 15:54:16 +0800

    update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -194,7 +194,7 @@
                 </div>
             </div>
         </li>
-        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+        {{-- <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->username }}</div>
             </a>
@@ -216,6 +216,45 @@
                 <a href="#" class="dropdown-item has-icon text-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+            </div>
+        </li> --}}
+
+        <li class="dropdown">
+            <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">
+                    @auth
+                        Hi, {{ Auth::user()->username }}
+                    @else
+                        Hi, Guest
+                    @endauth
+                </div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                @auth
+                    <div class="dropdown-title">Logged in 5 min ago</div>
+                    <a href="features-profile.html" class="dropdown-item has-icon">
+                        <i class="far fa-user"></i> Profile
+                    </a>
+                    <a href="features-activities.html" class="dropdown-item has-icon">
+                        <i class="fas fa-bolt"></i> Activities
+                    </a>
+                    <a href="features-settings.html" class="dropdown-item has-icon">
+                        <i class="fas fa-cog"></i> Settings
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    <a href="#" class="dropdown-item has-icon text-danger" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                @else
+                    <div class="dropdown-title">You are not logged in</div>
+                    <a href="{{ route('login') }}" class="dropdown-item has-icon">
+                        <i class="fas fa-sign-in-alt"></i> Login
+                    </a>
+                @endauth
             </div>
         </li>
     </ul>
