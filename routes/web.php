6dc1cc7fc13b1d7295adab6d818c88a542fbfcdd
Windy Sabtami AJ <windysabtami85@gmail.com>
2024-10-11 22:42:58 +0800

    istrrr

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-// routes/web.php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -12,78 +10,64 @@ Route::redirect('/', '/dashboard-general-dashboard');
 
 // Auth Routes
 Route::middleware(['throttle:100,1'])->group(function () {
-    Route::get('/login', function () {
-        return view('auth.auth-login');
-    })->name('login');
-
+    Route::view('/login', 'auth.auth-login')->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 });
 
 // Register Route
-Route::get('/register', function () {
-    return view('auth.auth-register');
-})->name('register');
+Route::view('/register', 'auth.auth-register')->name('register');
 
 // Dashboard
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard-general-dashboard', function () {
-        return view('pages.dashboard-general-dashboard', ['type_menu' => 'dashboard']);
-    });
+    Route::view('/dashboard-general-dashboard', 'pages.dashboard-general-dashboard', ['type_menu' => 'dashboard']);
 
     // Gugatan
-    Route::get('/gugatan', [GugatanController::class, 'index'])->name('gugatan.index');
-    Route::get('/gugatan/create', [GugatanController::class, 'create'])->name('gugatan.create');
-    Route::post('/gugatan', [GugatanController::class, 'store'])->name('gugatan.store');
-    Route::get('/gugatan/{id}/edit', [GugatanController::class, 'edit'])->name('gugatan.edit');
-    Route::put('/gugatan/{id}', [GugatanController::class, 'update'])->name('gugatan.update');
-    Route::delete('/gugatan/{id}', [GugatanController::class, 'destroy'])->name('gugatan.destroy');
+    Route::prefix('gugatan')->name('gugatan.')->group(function () {
+        Route::get('/', [GugatanController::class, 'index'])->name('index');
+        Route::get('/create', [GugatanController::class, 'create'])->name('create');
+        Route::post('/', [GugatanController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [GugatanController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [GugatanController::class, 'update'])->name('update');
+        Route::delete('/{id}', [GugatanController::class, 'destroy'])->name('destroy');
+        Route::post('/page2', [GugatanController::class, 'page2'])->name('page2');
+        Route::get('/page2', [GugatanController::class, 'page2'])->name('page2.get');
+        Route::post('/page3', [GugatanController::class, 'page3'])->name('page3');
+        Route::get('/page3', [GugatanController::class, 'page3'])->name('page3.get');
+        Route::put('/{id}/page3', [GugatanController::class, 'page3'])->name('page3.update');
+        Route::post('/{id}/page2', [GugatanController::class, 'page2'])->name('edit.page2');
+        Route::post('/{id}/page3', [GugatanController::class, 'page3'])->name('edit.page3');
+        Route::get('/form', function () {
+            return view('pages.gugatan-form', ['type_menu' => 'gugatan']);
+        })->name('form');
+        Route::post('/store2', [GugatanController::class, 'store2'])->name('store2');
+        Route::post('/page2/store', function (Request $request) {
+            $request->session()->put('gugatan_form', $request->all());
+            return view('pages.gugatan-page2', ['type_menu' => 'gugatan']);
+        })->name('page2.store');
+        Route::post('/page3/store', function (Request $request) {
+            $request->session()->put('gugatan_page2', $request->all());
+            return view('pages.gugatan-page3', ['type_menu' => 'gugatan']);
+        })->name('page3.store');
+        Route::post('/store', [GugatanController::class, 'store'])->name('store.post');
+        Route::get('/sukses', function () {
+            $gugatan = session('gugatan');
+            if (!$gugatan) {
+                return redirect()->route('gugatan.form')->withErrors(['msg' => 'Data tidak ditemukan.']);
+            }
+            return view('pages.gugatan-sukses', ['type_menu' => 'gugatan', 'gugatan' => $gugatan]);
+        })->name('success');
+        Route::get('/{id}/generate-word', [GugatanController::class, 'generateWordDocument'])->name('generateWord');
+    });
 
-    Route::post('/gugatan/page2', [GugatanController::class, 'page2'])->name('gugatan.page2');
-    Route::get('/gugatan/page2', [GugatanController::class, 'page2'])->name('gugatan.page2.get');
-
-    Route::post('/gugatan/page3', [GugatanController::class, 'page3'])->name('gugatan.page3');
-    Route::get('/gugatan/page3', [GugatanController::class, 'page3'])->name('gugatan.page3.get');
-    Route::put('/gugatan/{id}/page3', [GugatanController::class, 'page3'])->name('gugatan.page3.update');
-
-    Route::post('/gugatan/{id}/page2', [GugatanController::class, 'page2'])->name('gugatan.edit.page2');
-    Route::post('/gugatan/{id}/page3', [GugatanController::class, 'page3'])->name('gugatan.edit.page3');
-
-    Route::get('/users', [UserController::class, 'index'])->name('user.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('user.create');
-    Route::post('/users', [UserController::class, 'store'])->name('user.store');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('user.update');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-
-    // Form Gugatan
-    Route::get('/gugatan-form', function () {
-        return view('pages.gugatan-form', ['type_menu' => 'gugatan']);
-    })->name('gugatan.form');
-    Route::post('/gugatan/store2', [GugatanController::class, 'store2'])->name('gugatan.store2');
-
-    Route::post('/gugatan-page2', function (Request $request) {
-        // Simpan data dari halaman pertama ke session
-        $request->session()->put('gugatan_form', $request->all());
-        return view('pages.gugatan-page2', ['type_menu' => 'gugatan']);
-    })->name('gugatan.page2.store');
-
-    Route::post('/gugatan-page3', function (Request $request) {
-        // Simpan data dari halaman kedua ke session
-        $request->session()->put('gugatan_page2', $request->all());
-        return view('pages.gugatan-page3', ['type_menu' => 'gugatan']);
-    })->name('gugatan.page3.store');
-
-    Route::post('/gugatan/store', [GugatanController::class, 'store'])->name('gugatan.store.post');
-
-    Route::get('/gugatan-sukses', function () {
-        $gugatan = session('gugatan');
-        if (!$gugatan) {
-            return redirect()->route('gugatan.form')->withErrors(['msg' => 'Data tidak ditemukan.']);
-        }
-        return view('pages.gugatan-sukses', ['type_menu' => 'gugatan', 'gugatan' => $gugatan]);
-    })->name('gugatan.success');
-
-    Route::get('/gugatan/{id}/generate-word', [GugatanController::class, 'generateWordDocument'])->name('gugatan.generateWord');
+    // Users
+    Route::prefix('users')->name('user.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::get('/create', [UserController::class, 'create'])->name('create');
+        Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('edit');
+        Route::put('/{user}', [UserController::class, 'update'])->name('update');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy');
+    });
 });
 
 // Layout
@@ -94,93 +78,11 @@ Route::view('/blank-page', 'pages.blank-page', ['type_menu' => '']);
 
 // Bootstrap
 $bootstrapRoutes = [
-    'alert',
-    'badge',
-    'breadcrumb',
-    'buttons',
-    'card',
-    'carousel',
-    'collapse',
-    'dropdown',
-    'form',
-    'list-group',
-    'media-object',
-    'modal',
-    'nav',
-    'navbar',
-    'pagination',
-    'popover',
-    'progress',
-    'table',
-    'tooltip',
-    'typography'
+    'alert', 'badge', 'breadcrumb', 'buttons', 'card', 'carousel', 'collapse', 'dropdown', 'form', 'list-group', 'media-object', 'modal', 'nav', 'navbar', 'pagination', 'popover', 'progress', 'table', 'tooltip', 'typography'
 ];
 foreach ($bootstrapRoutes as $route) {
     Route::view("/bootstrap-$route", "pages.bootstrap-$route", ['type_menu' => 'bootstrap']);
 }
-
-// // Components
-// $componentRoutes are [
-//     'article',
-//     'avatar',
-//     'chat-box',
-//     'empty-state',
-//     'gallery',
-//     'hero',
-//     'multiple-upload',
-//     'pricing',
-//     'statistic',
-//     'tab',
-//     'table',
-//     'user',
-//     'wizard'
-// ];
-// foreach ($componentRoutes as $route) {
-//     Route::view("/components-$route", "pages.components-$route", ['type_menu' => 'components']);
-// }
-
-// // Forms
-// $formRoutes are ['advanced-form', 'editor', 'validation'];
-// foreach ($formRoutes as $route) {
-//     Route::view("/forms-$route", "pages.forms-$route", ['type_menu' => 'forms']);
-// }
-
-// // Modules
-// $moduleRoutes are [
-//     'calendar',
-//     'chartjs',
-//     'datatables',
-//     'flag',
-//     'font-awesome',
-//     'ion-icons',
-//     'owl-carousel',
-//     'sparkline',
-//     'sweet-alert',
-//     'toastr',
-//     'vector-map',
-//     'weather-icon'
-// ];
-// foreach ($moduleRoutes as $route) {
-//     Route::view("/modules-$route", "pages.modules-$route", ['type_menu' => 'modules']);
-// }
-
-// // Error
-// $errorRoutes are ['403', '404', '500', '503'];
-// foreach ($errorRoutes as $route) {
-//     Route::view("/error-$route", "pages.error-$route", ['type_menu' => 'error']);
-// }
-
-// // Features
-// $featureRoutes are ['activities', 'post-create', 'post', 'profile', 'settings', 'setting-detail', 'tickets'];
-// foreach ($featureRoutes as $route) {
-//     Route::view("/features-$route", "pages.features-$route", ['type_menu' => 'features']);
-// }
-
-// // Utilities
-// $utilityRoutes are ['contact', 'invoice', 'subscribe'];
-// foreach ($utilityRoutes as $route) {
-//     Route::view("/utilities-$route", "pages.utilities-$route", ['type_menu' => 'utilities']);
-// }
 
 // Credits
 Route::view('/credits', 'pages.credits', ['type_menu' => '']);
